e6ce8a368784dffe3d8096f74fea3a95834fb37a
sepehram <sepehramooei@gmail.com>
2021-12-05 22:03:27 +0330

    mabani

---
+++ fibonacci.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long fibo(int n);
+
+int main()
+{
+     int number;
+     cout << "Enter your N-th number: ";
+     cin >> number;
+     cout << "Fibonacci"
+          << "(" << number << ")"
+          << "=" << fibo(number) << endl;
+     system("pause");
+     return 0;
+}
+long fibo(int n)
+{
+     int result;
+     if (n == 1 || n == 2)
+     {
+          return 1;
+     }
+     return fibo(n - 1) + fibo(n - 2);
+}
